5842bd6af425769cee347fadd06cd46227eae4d5
North Krimsly <north@highintegritydesign.com>
2013-11-30 18:47:43 -0800

    Got both inline and gist shortcodes working, with some testing.

--- hid-display-code.php
+++ hid-display-code.php
@@ -33,8 +33,9 @@ class HID_display_code {
             'do_display_code_shortcode')
         );  
 
-        // turn off 'texturize' for the shortcode. This will keep quotes as quotes
-        // rather than turning them into smartquotes. Similar for single quotes.
+        // turn off 'texturize' for the shortcode. This will keep double quotes as 
+        // double quotes rather than smart quotes, and keep single quotes frome being
+        // turned into apostrophies.
         add_filter( 'no_texturize_shortcodes', 'no_texturize_display_code_shortcode' );
         function no_texturize_display_code_shortcode($shortcodes){
             $shortcodes = array('hid-display-code');
@@ -46,18 +47,85 @@ class HID_display_code {
     {  
         // first parse the shortcode attributes and provide default values
         $args = shortcode_atts( array(
-            'type' => '',
-            'id' => '',
-            'file' => '',
-            'classes' => 'hid-display-code'
+            'user' => '',
+            'gist' => '',
+            'file' => ''
         ), $atts);
 
-        // for inline code, insert a span styled with any user-defined classes
-        // use class="hid-display-code" by default
-        if ($args['type'] == 'inline') {
-            $args['classes'] = esc_html($args['classes']);
-            $clean_content = esc_html($content);
-            return "<code class='" . $args['classes'] . "'>" . $clean_content . "</code>";
+        // Clean up the content and attributes
+        $args['user'] = esc_html(trim($args['user']));                
+        $args['gist'] = esc_html(trim($args['gist']));        
+        $args['file'] = esc_html(trim($args['file']));                
+        $content = esc_html($content);
+
+        // Save boolean conditions for complex logic later
+        $have_user = !empty($args['user']);
+        $have_gist = !empty($args['gist']);
+        $have_file = !empty($args['file']);
+        $have_content = !empty($content);
+        $valid_user = preg_match("/^[a-z0-9]+[a-z0-9-]*$/i", $args['user']);
+        $valid_gist = preg_match("/^\d+$/", $args['gist']);
+
+        // Check if $content is supplied, but also a gist ID or user name or file name.
+        // If so, that's an error.
+        if (($have_content) && (($have_gist) || $have_user || $have_file)) {
+            if ( true === WP_DEBUG ) {
+                error_log('hid-display-code plugin: cannot specify both a gist and inline code content.');
+            }
+            return;
+        }
+
+        // Check if only one of gist ID and user name are supplied. 
+        // If only one is supplied, it's an error.
+        if (($have_gist && !$have_user) || ($have_user && !$have_gist)) {
+            if ( true === WP_DEBUG ) {
+                error_log('hid-display-code plugin: both a Github username and gist ID must be specified.');
+            }
+            return;
+        }
+
+        // Check if nothing was supplied at all
+        if (!$have_gist && !$have_user && !$have_content) {
+            if ( true === WP_DEBUG ) {
+                error_log('hid-display-code plugin: either a Gist or inline content must be specified.');
+            }
+            return;
+        }
+
+        // if the user name or Gist ID aren't valid, return an error
+        if ($have_gist && !$valid_user) {
+            if ( true === WP_DEBUG ) {
+                error_log('hid-display-code plugin: invalid Github username.');
+            }
+            return;
+
+        }
+        if ($have_gist && !$valid_gist) {
+            if ( true === WP_DEBUG ) {
+                error_log('hid-display-code plugin: invalid Gist ID.');
+            }
+            return;
+
+        }
+
+        // If it's a valid username and Gist ID, embed Javascript to display the Gist
+        if ($have_gist) {
+            $html = '<script src="http://gist.github.com/' . $args['user'] . '/'
+                    . $args['gist'] . '.js';
+
+            // if a file is specified then build that into the embed as well
+            if ($have_file) {
+                $html = $html . '?file=' . $args['file'];
+            }
+
+            // finish the embed reference and return it
+            $html = $html . '"></script>';
+            return $html;
+        }
+
+        // or else it's inline code, so insert a <code> element
+        else {
+            return "<code>" . $content . "</code>";            
         }
     }
 }
